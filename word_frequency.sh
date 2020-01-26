e837c405e08fd1fb3d2d3e49a6e126dfe3734dad
Andy Cai <andylei.cai@gmail.com>
2020-01-25 19:52:00 -0800

    fix sorted by reverse

--- word_frequency.sh
+++ word_frequency.sh
@@ -11,5 +11,11 @@
 # the words.txt has the following content:
 #    the day is sunny the the
 #    the sunny is is
+# Your script should output the following, sorted by descending frequency:
+#   the 4
+#   is 3
+#   sunny 2
+#   day 1
 
-cat words.txt | while read line;do echo $line | awk '{for(i=1;i<=NF;i++) print $i}' ;done | sort | uniq -c | awk '{print $2" "$1}'
+ # finally use awk '{print | "$1 sort -r"}' print out the results sorted by descending frequency number
+cat words.txt | while read line;do echo $line | awk '{for(i=1;i<=NF;i++) print $i}' ;done | sort | uniq -c | awk '{print |"$1 sort -r"}' | awk '{print $2" "$1}'
